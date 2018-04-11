cb047dfdc571837dace182c7b5951377fb04bdd9
adi <adi@barak.com>
2018-04-11 19:27:02 +0300

    fixes

--- ex0/app/src/main/java/com/brock/adi/tapcounter/MainActivity.java
+++ ex0/app/src/main/java/com/brock/adi/tapcounter/MainActivity.java
@@ -7,25 +7,47 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String KEY_COUNT = "count";
     int counter = 0;
+    TextView counterText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView counter = findViewById(R.id.counterText);
-        counter.setText(R.string.press_button_to_start);
-        counter.setTextSize(24);
+        if (savedInstanceState != null){
+            counter = savedInstanceState.getInt(KEY_COUNT);
+        }
 
-        Button counterButton = findViewById(R.id.counterButton);
-        counterButton.setOnClickListener(new View.OnClickListener(){
+        counterText = findViewById(R.id.counterText);
+//        counterText.setTextSize(64);
+        setNumberOfTaps();
+
+
+//        counterText.setText(R.string.press_button_to_start);
+//        counterText.setTextSize(24);
+
+//        Button counterButton = findViewById(R.id.counterButton);
+//        counterButton.setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.counterButton).setOnClickListener(new View.OnClickListener(){;
                 @Override
                 public void onClick(View v){
-                counter.setText("" + (++MainActivity.this.counter));
-                counter.setTextSize(64);
+//                counterText.setText("" + (++MainActivity.this.counter));
+//                    counterText.setTextSize(64);
+                    counter ++;
+                    setNumberOfTaps();
                 }
         });
+    }
+
+    private void setNumberOfTaps(){
+        counterText.setText(String.valueOf(counter));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        outState.putInt(KEY_COUNT, counter);
+        super.onSaveInstanceState(outState);
     }
 }
