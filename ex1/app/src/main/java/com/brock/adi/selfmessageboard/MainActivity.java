1ae8834495e2efff12d2b96e47a973a8764c22c5
adi <adi@barak.com>
2018-04-18 10:51:39 +0300

    ex1 done

--- ex1/app/src/main/java/com/brock/adi/selfmessageboard/MainActivity.java
+++ ex1/app/src/main/java/com/brock/adi/selfmessageboard/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,36 +14,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MessageAdapter messageAdapter = new MessageAdapter();
+        final MessageAdapter messageAdapter = new MessageAdapter();
         RecyclerView recyclerView = findViewById(R.id.recycler);
         recyclerView.setAdapter(messageAdapter);
         RecyclerView.LayoutManager manager = new LinearLayoutManager(this,
                 LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(manager);
 
-        messageAdapter.addMessage("hello");
-        messageAdapter.addMessage("world");
-        messageAdapter.addMessage("i");
-        messageAdapter.addMessage("love");
-        messageAdapter.addMessage("you");
-        messageAdapter.addMessage("my");
-        messageAdapter.addMessage("beautiful");
-        messageAdapter.addMessage("cookie");
-        messageAdapter.addMessage("who");
-        messageAdapter.addMessage("is");
-        messageAdapter.addMessage("my");
-        messageAdapter.addMessage("amazing");
-        messageAdapter.addMessage("husband");
-        messageAdapter.addMessage("in");
-        messageAdapter.addMessage("the");
-        messageAdapter.addMessage("whole");
-        messageAdapter.addMessage("wide");
-        messageAdapter.addMessage("world");
+        final EditText edit = findViewById(R.id.editText);
 
-
-        // View.OnClickListener
-
-
+        findViewById(R.id.sendButton).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String message = edit.getText().toString();
+                messageAdapter.addMessage(message);
+                edit.setText("");
+            }
+        });
 
     }
 }
