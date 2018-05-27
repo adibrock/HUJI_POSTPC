2f9f7a533a9f2d9e0f749eb5127770284a7d9551
adi <adi@barak.com>
2018-05-27 23:48:17 +0300

    images in recyclerview

--- ex4/app/src/main/java/com/brock/adi/imagedownloader/ImageAdapter.java
+++ ex4/app/src/main/java/com/brock/adi/imagedownloader/ImageAdapter.java
@@ -2,11 +2,19 @@ package com.brock.adi.imagedownloader;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder>{
+//import com.squareup.picasso.Picasso;
+
+import com.bumptech.glide.Glide;
+//import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
 
     public class ImageViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
@@ -16,20 +24,30 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
         }
     }
 
+    ArrayList<String> imageUrls;
+
+    public ImageAdapter(ArrayList<String> imageUrls) {
+        this.imageUrls = imageUrls;
+    }
+
+
+
     @NonNull
     @Override
     public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.item_image, parent, false);
+        return new ImageViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
-
+            GlideApp.with(holder.imageView.getContext()).load(imageUrls.get(position)).into(holder.imageView);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return imageUrls.size();
     }
 
 
--- ex4/app/src/main/java/com/brock/adi/imagedownloader/MainActivity.java
+++ ex4/app/src/main/java/com/brock/adi/imagedownloader/MainActivity.java
@@ -2,15 +2,29 @@ package com.brock.adi.imagedownloader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> imageUrls = new ArrayList<>();
+        imageUrls.add("http://i.imgur.com/jGkaxEZ.jpg");
+        imageUrls.add("http://i.imgur.com/cuS5DDv.jpg");
+        imageUrls.add("http://i.imgur.com/bl5DOR0.jpg");
+
+        adapter = new ImageAdapter(imageUrls);
+        RecyclerView imageRecyclerView = findViewById(R.id.recyclerView);
+        imageRecyclerView.setAdapter(adapter);
+        imageRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
 
         findViewById(R.id.downloadButton).setOnClickListener(new View.OnClickListener() {
             @Override
